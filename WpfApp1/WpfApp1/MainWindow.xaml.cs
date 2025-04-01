3c9b2a083107b9935f29d9b84fd35f1e8f6758d9
Suhas Santosh Pai <suhaspai412@gmail.com>
2025-04-01 21:39:57 +0530

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace WpfSplitDropdown
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DropDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            button.ContextMenu.IsOpen = true;
+        }
+
+        private void SplitButton_Toggle_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton toggleButton = sender as ToggleButton;
+            toggleButton.ContextMenu.IsOpen = true;
+        }
+
+        private void SplitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Main Action Executed!");
+        }
+    }
+}
